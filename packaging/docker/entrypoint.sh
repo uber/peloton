e30b649929b884a96aa023f8d97b43e06f426324
Jimmy Wu <wu@uber.com>
2017-01-04 16:55:06 -0800

    Fix peloton container entrypoint script to provide --config flag
    Summary: With changes from D687501, flag '--config' becomes mandatory when running peloton master, this breaks Peloton docker entrypoint, which still attempts to read config from env. This is a quick fix in entrypoint script, we need to land it to unblock peloton deployment to production clusters.
    
    Test Plan: tested building and launching peloton container locally
    
    Reviewers: min, gconradi, igor, #peloton_meta_framework, slu
    
    Reviewed By: #peloton_meta_framework, slu
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D691355

--- packaging/docker/entrypoint.sh
+++ packaging/docker/entrypoint.sh
@@ -2,4 +2,5 @@
 
 set -euxo pipefail
 
-/usr/bin/peloton/peloton-master
+/usr/bin/peloton/peloton-master -c $UBER_CONFIG_DIR/base.yaml \
+                                -c $UBER_CONFIG_DIR/$UBER_ENVIRONMENT.yaml
