5cc40c3d5cc8ad0a1eee886e5e40ee9c114ad619
Shi Lu <slu@uber.com>
2017-02-23 11:07:13 -0800

    Add circular buffer implemetation struct
    Summary: Add circular buffer implemetation struct
    
    Test Plan: New testcase added
    
    Reviewers: min, zhitao, wu, #peloton
    
    Reviewed By: zhitao, wu, #peloton
    
    Subscribers: mabansal, mu, xiaojian, jenkins
    
    Differential Revision: https://code.uberinternal.com/D749491

---
+++ common/cirbuf/circular_buffer.go
@@ -0,0 +1,129 @@
+package cirbuf
+
+import (
+	"errors"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"sync"
+)
+
+// CircularBuffer is a circular buffer implementation
+type CircularBuffer struct {
+	sync.RWMutex
+	buffer []*CircularBufferItem
+	// head points to the next newest item to be added
+	head uint64
+	// tail points to the last available item
+	tail uint64
+}
+
+// NewCircularBuffer creates an circular buffer with size bufferSize
+func NewCircularBuffer(bufferSize int) *CircularBuffer {
+	buffer := make([]*CircularBufferItem, bufferSize)
+	return &CircularBuffer{
+		buffer: buffer,
+	}
+}
+
+// Capacity returns the total capacity of the circular buffer
+func (c *CircularBuffer) Capacity() int {
+	c.RLock()
+	defer c.RUnlock()
+	return len(c.buffer)
+}
+
+// Size returns the size used in the circular buffer
+func (c *CircularBuffer) Size() int {
+	c.RLock()
+	defer c.RUnlock()
+	return int(c.head - c.tail)
+}
+
+// GetItem returns the CircularBufferItem located by sequence id
+func (c *CircularBuffer) GetItem(sequence uint64) (*CircularBufferItem, error) {
+	c.RLock()
+	defer c.RUnlock()
+	if sequence < c.tail || sequence > c.head {
+		log.WithField("position", sequence).
+			WithField("head", c.head).
+			WithField("tail", c.tail).
+			Error("Index out of bound")
+		return nil, errors.New("Index out of bound")
+	}
+	return c.buffer[int(sequence%uint64(len(c.buffer)))], nil
+}
+
+// GetItemsByRange returns the CircularBufferItems located by sequence id. Both from and to are inclusive
+// TODO: consider adding a best effort version of this function that returns partial content and no error
+func (c *CircularBuffer) GetItemsByRange(from uint64, to uint64) ([]*CircularBufferItem, error) {
+	c.RLock()
+	defer c.RUnlock()
+	if from < c.tail || from >= c.head || to < c.tail || to >= c.head {
+		log.WithField("from", from).
+			WithField("to", to).
+			WithField("head", c.head).
+			WithField("tail", c.tail).
+			Error("Index out of bound")
+		return nil, errors.New("Index out of bound")
+	}
+	var items []*CircularBufferItem
+	for i := from; i <= to; i++ {
+		items = append(items, c.buffer[int(i%uint64(len(c.buffer)))])
+	}
+	return items, nil
+}
+
+// AddItem add a data record into the circular buffer, returns the CircularBufferItem created
+func (c *CircularBuffer) AddItem(data interface{}) (*CircularBufferItem, error) {
+	c.Lock()
+	defer c.Unlock()
+	if c.isFull() {
+		log.WithField("head", c.head).
+			WithField("tail", c.tail).
+			Error("Buffer is full")
+		return nil, fmt.Errorf("Buffer is full")
+	}
+	item := &CircularBufferItem{
+		SequenceID: c.head,
+		Value:      data,
+	}
+	c.buffer[int((c.head)%uint64(len(c.buffer)))] = item
+	c.head++
+	return item, nil
+}
+
+// MoveTail increases the tail position, and return the list of CircularBufferItem moved out
+func (c *CircularBuffer) MoveTail(newTail uint64) ([]*CircularBufferItem, error) {
+	c.Lock()
+	defer c.Unlock()
+	if newTail > c.head || newTail < c.tail {
+		log.WithField("head", c.head).
+			WithField("tail", c.tail).
+			Error("New tail value is out of range")
+		return nil, fmt.Errorf("New tail value is out of range")
+	}
+	var removedItems []*CircularBufferItem
+	for i := c.tail; i < newTail; i++ {
+		removedItems = append(removedItems, c.buffer[int(i%uint64(len(c.buffer)))])
+	}
+	c.tail = newTail
+	return removedItems, nil
+}
+
+// Note: not thread safe and need to be called with lock
+func (c *CircularBuffer) isFull() bool {
+	return int(c.head-c.tail) >= len(c.buffer)
+}
+
+// GetRange returns the head / tail of the circular buffer
+func (c *CircularBuffer) GetRange() (uint64, uint64) {
+	c.RLock()
+	defer c.RUnlock()
+	return c.head, c.tail
+}
+
+// CircularBufferItem is the item stored in the circular buffer
+type CircularBufferItem struct {
+	SequenceID uint64
+	Value      interface{}
+}
