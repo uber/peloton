4925123e68c5e372a34f03ef0491ca7aa669afb6
Tengfei Mu <mu@uber.com>
2017-03-17 17:21:34 -0700

    make reconciler start/stop no-op if already started/stopped
    Summary:
    current lostleadershipcallback() happens before gainleadershipcallback(), we need to
    make sure reconciler do not stop if already stopped.
    
    Reviewers: slu, min, #peloton
    
    Reviewed By: slu, #peloton
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D779902

--- hostmgr/reconcile/task_reconciler.go
+++ hostmgr/reconcile/task_reconciler.go
@@ -2,6 +2,7 @@ package reconcile
 
 import (
 	"errors"
+	"sync"
 	"time"
 
 	mesos "mesos/v1"
@@ -11,6 +12,7 @@ import (
 	"code.uber.internal/infra/peloton/yarpc/encoding/mpb"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/uber-go/atomic"
 	"github.com/uber-go/tally"
 )
 
@@ -23,6 +25,9 @@ type TaskReconciler interface {
 
 // taskReconciler implements TaskReconciler.
 type taskReconciler struct {
+	sync.Mutex
+
+	Running                   *atomic.Bool
 	client                    mpb.Client
 	metrics                   *Metrics
 	frameworkInfoProvider     hostmgr_mesos.FrameworkInfoProvider
@@ -47,6 +52,7 @@ func InitTaskReconciler(
 		return
 	}
 	reconciler = &taskReconciler{
+		Running:                   atomic.NewBool(false),
 		client:                    client,
 		metrics:                   NewMetrics(parent.SubScope("reconcile")),
 		frameworkInfoProvider:     frameworkInfoProvider,
@@ -67,8 +73,15 @@ func GetTaskReconciler() TaskReconciler {
 // Start initiates implicit task reconciliation.
 func (r *taskReconciler) Start() {
 	log.Info("Reconcile tasks start called.")
+	r.Lock()
+	defer r.Unlock()
+	if r.Running.Swap(true) {
+		log.Info("Task reconciler is already running, no-op.")
+		return
+	}
 	// TODO: add stats for # of reconciliation updates.
 	go func() {
+		defer r.Running.Store(false)
 		time.Sleep(r.initialReconcileDelay)
 		err := r.reconcileImplicitly()
 		if err != nil {
@@ -81,7 +94,7 @@ func (r *taskReconciler) Start() {
 		for {
 			select {
 			case <-r.stopChan:
-				log.Info("Stopping periodical implicit task reconciliation.")
+				log.Info("periodical implicit task reconciliation stopped.")
 				return
 			case t := <-ticker.C:
 				log.WithField("tick", t).
@@ -99,10 +112,25 @@ func (r *taskReconciler) Start() {
 // Stop stops implicit task reconciliation.
 func (r *taskReconciler) Stop() {
 	log.Info("Stop implicitly reconcile tasks called.")
+	r.Lock()
+	defer r.Unlock()
+
+	if !r.Running.Load() {
+		log.Warn("Task reconciler is not running, no-op.")
+		return
+	}
+
+	log.Info("Stopping implicitly reconcile tasks.")
 	r.stopChan <- struct{}{}
+
+	for r.Running.Load() {
+		time.Sleep(1 * time.Millisecond)
+	}
+	log.Info("Stop implicitly reconcile tasks returned.")
 }
 
 func (r *taskReconciler) reconcileImplicitly() error {
+	log.Info("Reconcile tasks implicitly called.")
 	frameworkID := r.frameworkInfoProvider.GetFrameworkID()
 	streamID := r.frameworkInfoProvider.GetMesosStreamID()
 
--- hostmgr/reconcile/task_reconciler_test.go
+++ hostmgr/reconcile/task_reconciler_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
+	"github.com/uber-go/atomic"
 	"github.com/uber-go/tally"
 
 	mesos "mesos/v1"
@@ -40,6 +41,7 @@ func TestInitImplicitTaskReconcilationOnce(t *testing.T) {
 	mockMesosClient := mock_mpb.NewMockClient(ctrl)
 	mtx := NewMetrics(tally.NoopScope)
 	reconciler := &taskReconciler{
+		Running:                   atomic.NewBool(false),
 		client:                    mockMesosClient,
 		metrics:                   mtx,
 		frameworkInfoProvider:     &mockFrameworkInfoProvider{},
@@ -71,6 +73,7 @@ func TestInitImplicitTaskReconcilationWithPeriodicalCalls(t *testing.T) {
 	mockMesosClient := mock_mpb.NewMockClient(ctrl)
 	mtx := NewMetrics(tally.NoopScope)
 	reconciler := &taskReconciler{
+		Running:                   atomic.NewBool(false),
 		client:                    mockMesosClient,
 		metrics:                   mtx,
 		frameworkInfoProvider:     &mockFrameworkInfoProvider{},
@@ -103,6 +106,7 @@ func TestInitImplicitTaskReconcilationPeriodicalFailure(t *testing.T) {
 	mockMesosClient := mock_mpb.NewMockClient(ctrl)
 	mtx := NewMetrics(tally.NoopScope)
 	reconciler := &taskReconciler{
+		Running:                   atomic.NewBool(false),
 		client:                    mockMesosClient,
 		metrics:                   mtx,
 		frameworkInfoProvider:     &mockFrameworkInfoProvider{},
@@ -129,6 +133,41 @@ func TestInitImplicitTaskReconcilationPeriodicalFailure(t *testing.T) {
 	reconciler.Start()
 	time.Sleep(450 * time.Millisecond)
 	assert.Equal(t, len(reconciler.stopChan), 0)
+	reconciler.Stop()
+	time.Sleep(100 * time.Millisecond)
+	assert.Equal(t, len(reconciler.stopChan), 0)
+}
+
+func TestInitImplicitTaskReconcilationNotStartIfAlreadyRunning(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockMesosClient := mock_mpb.NewMockClient(ctrl)
+	mtx := NewMetrics(tally.NoopScope)
+	reconciler := &taskReconciler{
+		Running:                   atomic.NewBool(false),
+		client:                    mockMesosClient,
+		metrics:                   mtx,
+		frameworkInfoProvider:     &mockFrameworkInfoProvider{},
+		initialReconcileDelay:     initialReconcileDelay,
+		implicitReconcileInterval: implicitReconcileInterval,
+		stopChan:                  make(chan struct{}, 1),
+	}
+
+	gomock.InOrder(
+		mockMesosClient.EXPECT().
+			Call(
+				gomock.Eq(streamID),
+				gomock.Any()).
+			Return(nil),
+	)
+
+	reconciler.Stop()
+	reconciler.Start()
+	reconciler.Start()
+	time.Sleep(350 * time.Millisecond)
+	assert.Equal(t, len(reconciler.stopChan), 0)
+	reconciler.Stop()
 	reconciler.Stop()
 	time.Sleep(100 * time.Millisecond)
 	assert.Equal(t, len(reconciler.stopChan), 0)
