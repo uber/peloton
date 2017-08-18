00ade6f582fd6b0af47d978848f798751ac81721
Mayank Bansal <mabansal@uber.com>
2017-08-17 23:48:24 -0700

    JobManager should call Resmgr Kill API's
    Summary:
    Job Manager should call Resourcemanager kill appi when trying to reach goal state
    Before calling Hostmanager kill it should also call resmgr kill
    
    Test Plan: test added
    
    Reviewers: mu, min, avyas, #peloton, ajohnsen
    
    Reviewed By: #peloton, ajohnsen
    
    Subscribers: ajohnsen, jenkins
    
    Differential Revision: https://code.uberinternal.com/D1123037

--- jobmgr/task/goalstate/task_operator.go
+++ jobmgr/task/goalstate/task_operator.go
@@ -4,11 +4,19 @@ import (
 	"context"
 	"fmt"
 
+	"go.uber.org/yarpc"
+
+	"github.com/pkg/errors"
+
 	"code.uber.internal/infra/peloton/.gen/mesos/v1"
+	"code.uber.internal/infra/peloton/.gen/peloton/api/peloton"
 	"code.uber.internal/infra/peloton/.gen/peloton/api/task"
 	"code.uber.internal/infra/peloton/.gen/peloton/private/hostmgr/hostsvc"
+	"code.uber.internal/infra/peloton/.gen/peloton/private/resmgrsvc"
+
 	"code.uber.internal/infra/peloton/common"
-	"go.uber.org/yarpc"
+	"code.uber.internal/infra/peloton/util"
+	"github.com/sirupsen/logrus"
 )
 
 // TaskOperator can perform operations on a task, potentially resulting in the
@@ -23,15 +31,41 @@ type TaskOperator interface {
 func NewTaskOperator(d *yarpc.Dispatcher) TaskOperator {
 	return &taskOperator{
 		hostmgrClient: hostsvc.NewInternalHostServiceYARPCClient(d.ClientConfig(common.PelotonHostManager)),
+		resmgrClient:  resmgrsvc.NewResourceManagerServiceYARPCClient(d.ClientConfig(common.PelotonResourceManager)),
 	}
 }
 
 type taskOperator struct {
 	hostmgrClient hostsvc.InternalHostServiceYARPCClient
+	resmgrClient  resmgrsvc.ResourceManagerServiceYARPCClient
 }
 
+//
 func (o *taskOperator) StopTask(ctx context.Context, runtime *task.RuntimeInfo) error {
-	// TODO(mu): Notify RM to also remove these tasks from task queue.
+	// We need to parse the peloton task id from Mesos taskId
+	pelotonTaskID, err := util.ParseTaskIDFromMesosTaskID(
+		runtime.MesosTaskId.GetValue())
+	if err != nil {
+		return err
+	}
+
+	taskID := &peloton.TaskID{
+		Value: pelotonTaskID,
+	}
+	killReq := &resmgrsvc.KillTasksRequest{
+		Tasks: []*peloton.TaskID{taskID},
+	}
+	// Calling resmgr Kill API
+	killRes, err := o.resmgrClient.KillTasks(ctx, killReq)
+	if err != nil {
+		logrus.WithError(err).Error("Error in killing from resmgr")
+	}
+
+	if killRes.Error != nil {
+		logrus.WithError(errors.New(killRes.GetError().Message)).
+			Error("Error in killing from resmgr")
+	}
+
 	req := &hostsvc.KillTasksRequest{
 		TaskIds: []*mesos_v1.TaskID{runtime.GetMesosTaskId()},
 	}
--- jobmgr/task/goalstate/task_operator_test.go
+++ jobmgr/task/goalstate/task_operator_test.go
@@ -5,9 +5,14 @@ import (
 	"testing"
 
 	"code.uber.internal/infra/peloton/.gen/mesos/v1"
+	"code.uber.internal/infra/peloton/.gen/peloton/api/peloton"
 	"code.uber.internal/infra/peloton/.gen/peloton/api/task"
 	"code.uber.internal/infra/peloton/.gen/peloton/private/hostmgr/hostsvc"
+	"code.uber.internal/infra/peloton/.gen/peloton/private/resmgrsvc"
+
 	host_mocks "code.uber.internal/infra/peloton/.gen/peloton/private/hostmgr/hostsvc/mocks"
+	resmgr_mocks "code.uber.internal/infra/peloton/.gen/peloton/private/resmgrsvc/mocks"
+
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
@@ -15,18 +20,28 @@ import (
 func TestTaskOperatorStop(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	hostMock := host_mocks.NewMockInternalHostServiceYARPCClient(ctrl)
+	resMgrMock := resmgr_mocks.NewMockResourceManagerServiceYARPCClient(ctrl)
 
 	to := &taskOperator{
 		hostmgrClient: hostMock,
+		resmgrClient:  resMgrMock,
 	}
 
 	taskID := &mesos_v1.TaskID{
 		Value: &[]string{"3c8a3c3e-71e3-49c5-9aed-2929823f595c-1-3c8a3c3e-71e3-49c5-9aed-2929823f5957"}[0],
 	}
 
+	pelotonTaskID := &peloton.TaskID{
+		Value: "3c8a3c3e-71e3-49c5-9aed-2929823f595c-1",
+	}
+
 	hostMock.EXPECT().KillTasks(context.Background(), &hostsvc.KillTasksRequest{
 		TaskIds: []*mesos_v1.TaskID{taskID},
 	}).Return(nil, nil)
+
+	resMgrMock.EXPECT().KillTasks(context.Background(), &resmgrsvc.KillTasksRequest{
+		Tasks: []*peloton.TaskID{pelotonTaskID},
+	}).Return(&resmgrsvc.KillTasksResponse{}, nil)
 
 	assert.NoError(t, to.StopTask(context.Background(), &task.RuntimeInfo{
 		MesosTaskId: taskID,
