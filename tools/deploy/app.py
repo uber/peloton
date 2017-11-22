20bd124c03c00a0638918f6ee0a5b82667c536bc
Tengfei Mu <mu@uber.com>
2017-11-21 16:05:08 -0800

    Mount secrets if not empty
    Summary: att
    
    Reviewers: min, apoorvaj, #peloton
    
    Reviewed By: apoorvaj, #peloton
    
    Differential Revision: https://code.uberinternal.com/D1341157

--- tools/deploy/app.py
+++ tools/deploy/app.py
@@ -167,7 +167,7 @@ class App(object):
         ]
 
         # Mount langley secrets if secret dir is specified.
-        if not getattr(self.cluster, 'secret_config_dir', ''):
+        if getattr(self.cluster, 'secret_config_dir', ''):
             volumes.append((
                 self.cluster.secret_config_dir,
                 self.cluster.secret_config_dir,
