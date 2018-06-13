605616aeacd250fa13302ba2104107055dda4052
Zhixin Wen <zhixin@uber.com>
2018-06-12 17:02:59 -0700

    Refactor task_test.go to use test suite
    Summary:
    Current code is hard to add unit tests, refactor the code to use test suite.
    This lays the foundation to add tests for task write-through cache.
    
    Reviewers: #peloton, apoorvaj, mu
    
    Reviewed By: #peloton, apoorvaj
    
    Subscribers: jenkins
    
    Maniphest Tasks: T1832875
    
    Differential Revision: https://code.uberinternal.com/D1815833

--- jobmgr/cached/task_test.go
+++ jobmgr/cached/task_test.go
@@ -14,7 +14,7 @@ import (
 
 	"github.com/golang/mock/gomock"
 	"github.com/pborman/uuid"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"github.com/uber-go/tally"
 )
 
@@ -45,18 +45,42 @@ func initializeTaskRuntime(state pbtask.TaskState, version uint64) *pbtask.Runti
 	return runtime
 }
 
-// TestTask tests fetching the identifiers, the state and goal state from the task
-func TestTask(t *testing.T) {
-	jobID := peloton.JobID{Value: uuid.NewRandom().String()}
-	instanceID := uint32(1)
+type TaskTestSuite struct {
+	suite.Suite
 
+	ctrl       *gomock.Controller
+	jobID      *peloton.JobID
+	instanceID uint32
+	taskStore  *storemocks.MockTaskStore
+}
+
+func (suite *TaskTestSuite) SetupTest() {
+	suite.jobID = &peloton.JobID{Value: uuid.NewRandom().String()}
+
+	suite.instanceID = uint32(1)
+
+	suite.ctrl = gomock.NewController(suite.T())
+	suite.taskStore = storemocks.NewMockTaskStore(suite.ctrl)
+}
+
+func (suite *TaskTestSuite) TearDownTest() {
+	suite.ctrl.Finish()
+}
+
+func TestTask(t *testing.T) {
+	suite.Run(t, new(TaskTestSuite))
+}
+
+// TestTaskUpdateRuntimeCacheOnly tests fetching the identifiers,
+// the state and goal state from the task
+func (suite *TaskTestSuite) TestTaskUpdateRuntimeCacheOnly() {
 	tt := &task{
-		id:    instanceID,
-		jobID: &jobID,
+		id:    suite.instanceID,
+		jobID: suite.jobID,
 	}
 
-	assert.Equal(t, instanceID, tt.ID())
-	assert.Equal(t, jobID, *tt.JobID())
+	suite.Equal(suite.instanceID, tt.ID())
+	suite.Equal(suite.jobID.Value, tt.jobID.Value)
 
 	// Test fetching state and goal state of task
 	runtime := pbtask.RuntimeInfo{
@@ -67,115 +91,81 @@ func TestTask(t *testing.T) {
 
 	curState := tt.CurrentState()
 	curGoalState := tt.GoalState()
-	assert.Equal(t, runtime.State, curState.State)
-	assert.Equal(t, runtime.GoalState, curGoalState.State)
+	suite.Equal(runtime.State, curState.State)
+	suite.Equal(runtime.GoalState, curGoalState.State)
 }
 
 // TestTaskValidateFailRevision tests that updating a runtime with invalid revision fails
-func TestTaskValidateFailRevision(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	taskstore := storemocks.NewMockTaskStore(ctrl)
+func (suite *TaskTestSuite) TestTaskValidateFailRevision() {
 	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
 	instID := uint32(1)
 	runtime := initializeTaskRuntime(pbtask.TaskState_RUNNING, 2)
-	tt := initializeTask(taskstore, jobID, instID, runtime)
+	tt := initializeTask(suite.taskStore, jobID, instID, runtime)
 
 	newRuntime := initializeTaskRuntime(pbtask.TaskState_LAUNCHED, 1)
-	assert.False(t, tt.validateStateUpdate(newRuntime))
+	suite.False(tt.validateStateUpdate(newRuntime))
 
 	newRuntime = initializeTaskRuntime(pbtask.TaskState_LAUNCHED, 2)
-	assert.False(t, tt.validateStateUpdate(newRuntime))
+	suite.False(tt.validateStateUpdate(newRuntime))
 }
 
 // TestTaskValidateFailMesosToRes tests that transitioning state from a mesos
 // owned state to a resource manager owned state fails
-func TestTaskValidateFailMesosToRes(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	taskstore := storemocks.NewMockTaskStore(ctrl)
-	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
-	instID := uint32(1)
+func (suite *TaskTestSuite) TestTaskValidateFailMesosToRes() {
 	runtime := initializeTaskRuntime(pbtask.TaskState_RUNNING, 2)
-	tt := initializeTask(taskstore, jobID, instID, runtime)
+	tt := initializeTask(suite.taskStore, suite.jobID, suite.instanceID, runtime)
 
 	newRuntime := &pbtask.RuntimeInfo{
 		State: pbtask.TaskState_PLACING,
 	}
-	assert.False(t, tt.validateStateUpdate(newRuntime))
+	suite.False(tt.validateStateUpdate(newRuntime))
 }
 
 // TestTaskValidateFailResToInitialized tests that transitioning state from a
 // resource manager owned state to INITIALIZED state fails
-func TestTaskValidateFailResToInitialized(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	taskstore := storemocks.NewMockTaskStore(ctrl)
-	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
-	instID := uint32(1)
+func (suite *TaskTestSuite) TestTaskValidateFailResToInitialized() {
 	runtime := initializeTaskRuntime(pbtask.TaskState_PLACING, 2)
-	tt := initializeTask(taskstore, jobID, instID, runtime)
+	tt := initializeTask(suite.taskStore, suite.jobID, suite.instanceID, runtime)
 
 	newRuntime := &pbtask.RuntimeInfo{
 		State: pbtask.TaskState_INITIALIZED,
 	}
-	assert.False(t, tt.validateStateUpdate(newRuntime))
+	suite.False(tt.validateStateUpdate(newRuntime))
 }
 
 // TestTaskValidateFailMesosToLaunched tests that transitioning state from a mesos
 // owned state to LAUNCHED state fails
-func TestTaskValidateFailMesosToLaunched(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	taskstore := storemocks.NewMockTaskStore(ctrl)
-	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
-	instID := uint32(1)
+func (suite *TaskTestSuite) TestTaskValidateFailMesosToLaunched() {
 	runtime := initializeTaskRuntime(pbtask.TaskState_RUNNING, 2)
-	tt := initializeTask(taskstore, jobID, instID, runtime)
+	tt := initializeTask(suite.taskStore, suite.jobID, suite.instanceID, runtime)
 
 	newRuntime := &pbtask.RuntimeInfo{
 		State: pbtask.TaskState_LAUNCHED,
 	}
-	assert.False(t, tt.validateStateUpdate(newRuntime))
+	suite.False(tt.validateStateUpdate(newRuntime))
 }
 
 // TestTaskValidateFailTerminalTaskUpdate tests that transitioning from a
 // terminal task state withouth changing the mesos task ID fails
-func TestTaskValidateFailTerminalTaskUpdate(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	taskstore := storemocks.NewMockTaskStore(ctrl)
-	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
-	instID := uint32(1)
+func (suite *TaskTestSuite) TestTaskValidateFailTerminalTaskUpdate() {
 	runtime := initializeTaskRuntime(pbtask.TaskState_SUCCEEDED, 2)
-	tt := initializeTask(taskstore, jobID, instID, runtime)
+	tt := initializeTask(suite.taskStore, suite.jobID, suite.instanceID, runtime)
 
 	newRuntime := &pbtask.RuntimeInfo{
 		State: pbtask.TaskState_RUNNING,
 	}
-	assert.False(t, tt.validateStateUpdate(newRuntime))
+	suite.False(tt.validateStateUpdate(newRuntime))
 }
 
 // TestTaskValidateMesosTaskIdChange tests state transition with a mesos
 // task identifier change
-func TestTaskValidateMesosTaskIdChange(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	taskstore := storemocks.NewMockTaskStore(ctrl)
-	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
-	instID := uint32(1)
+func (suite *TaskTestSuite) TestTaskValidateMesosTaskIdChange() {
 	mesosID := "old"
 	runtime := initializeTaskRuntime(pbtask.TaskState_FAILED, 2)
 	runtime.MesosTaskId = &mesosv1.TaskID{
 		Value: &mesosID,
 	}
-	tt := initializeTask(taskstore, jobID, instID, runtime)
+	tt := initializeTask(suite.taskStore, suite.jobID, suite.instanceID, runtime)
 
 	newMesosID := "new"
 	newRuntime := &pbtask.RuntimeInfo{
@@ -184,24 +174,18 @@ func TestTaskValidateMesosTaskIdChange(t *testing.T) {
 			Value: &newMesosID,
 		},
 	}
-	assert.True(t, tt.validateStateUpdate(newRuntime))
+	suite.True(tt.validateStateUpdate(newRuntime))
 }
 
 // TestTaskValidateFailOldMesosTaskId tests that transitioning to an
 // older mesos task identifier fails
-func TestTaskValidateFailOldMesosTaskId(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	taskstore := storemocks.NewMockTaskStore(ctrl)
-	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
-	instID := uint32(1)
+func (suite *TaskTestSuite) TestTaskValidateFailOldMesosTaskId() {
 	mesosID := "old"
 	runtime := initializeTaskRuntime(pbtask.TaskState_FAILED, 2)
 	runtime.MesosTaskId = &mesosv1.TaskID{
 		Value: &mesosID,
 	}
-	tt := initializeTask(taskstore, jobID, instID, runtime)
+	tt := initializeTask(suite.taskStore, suite.jobID, suite.instanceID, runtime)
 
 	newMesosID := "new"
 	newRuntime := &pbtask.RuntimeInfo{
@@ -210,35 +194,33 @@ func TestTaskValidateFailOldMesosTaskId(t *testing.T) {
 			Value: &newMesosID,
 		},
 	}
-	assert.False(t, tt.validateStateUpdate(newRuntime))
+	suite.False(tt.validateStateUpdate(newRuntime))
 }
 
 func validateMergeRuntime(
-	t *testing.T,
+	suite *TaskTestSuite,
 	newRuntime *pbtask.RuntimeInfo,
 	mergedRuntime *pbtask.RuntimeInfo,
 	expectedVersion uint64) {
-	assert.Equal(t, newRuntime.GetState(), mergedRuntime.GetState())
-	assert.Equal(t, newRuntime.GetMesosTaskId(), mergedRuntime.GetMesosTaskId())
-	assert.Equal(t, newRuntime.GetStartTime(), mergedRuntime.GetStartTime())
-	assert.Equal(t, newRuntime.GetCompletionTime(), mergedRuntime.GetCompletionTime())
-	assert.Equal(t, newRuntime.GetHost(), mergedRuntime.GetHost())
-	assert.Equal(t, newRuntime.GetPorts(), mergedRuntime.GetPorts())
-	assert.Equal(t, newRuntime.GetGoalState(), mergedRuntime.GetGoalState())
-	assert.Equal(t, newRuntime.GetMessage(), mergedRuntime.GetMessage())
-	assert.Equal(t, newRuntime.GetReason(), mergedRuntime.GetReason())
-	assert.Equal(t, newRuntime.GetFailureCount(), mergedRuntime.GetFailureCount())
-	assert.Equal(t, newRuntime.GetConfigVersion(), mergedRuntime.GetConfigVersion())
-	assert.Equal(t, newRuntime.GetDesiredConfigVersion(), mergedRuntime.GetDesiredConfigVersion())
-	assert.Equal(t, newRuntime.GetAgentID(), mergedRuntime.GetAgentID())
-	assert.Equal(t, newRuntime.GetPrevMesosTaskId(), mergedRuntime.GetPrevMesosTaskId())
-	assert.Equal(t, expectedVersion, mergedRuntime.Revision.Version)
+	suite.Equal(newRuntime.GetState(), mergedRuntime.GetState())
+	suite.Equal(newRuntime.GetMesosTaskId(), mergedRuntime.GetMesosTaskId())
+	suite.Equal(newRuntime.GetStartTime(), mergedRuntime.GetStartTime())
+	suite.Equal(newRuntime.GetCompletionTime(), mergedRuntime.GetCompletionTime())
+	suite.Equal(newRuntime.GetHost(), mergedRuntime.GetHost())
+	suite.Equal(newRuntime.GetPorts(), mergedRuntime.GetPorts())
+	suite.Equal(newRuntime.GetGoalState(), mergedRuntime.GetGoalState())
+	suite.Equal(newRuntime.GetMessage(), mergedRuntime.GetMessage())
+	suite.Equal(newRuntime.GetReason(), mergedRuntime.GetReason())
+	suite.Equal(newRuntime.GetFailureCount(), mergedRuntime.GetFailureCount())
+	suite.Equal(newRuntime.GetConfigVersion(), mergedRuntime.GetConfigVersion())
+	suite.Equal(newRuntime.GetDesiredConfigVersion(), mergedRuntime.GetDesiredConfigVersion())
+	suite.Equal(newRuntime.GetAgentID(), mergedRuntime.GetAgentID())
+	suite.Equal(newRuntime.GetPrevMesosTaskId(), mergedRuntime.GetPrevMesosTaskId())
+	suite.Equal(expectedVersion, mergedRuntime.Revision.Version)
 }
 
 // TestTaskMergeRuntime tests merging current and new runtime
-func TestTaskMergeRuntime(t *testing.T) {
-	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
-	instID := uint32(1)
+func (suite *TaskTestSuite) TestTaskMergeRuntime() {
 	mesos1Old := "mesos1-old"
 	mesos1Older := "mesos1-older"
 	agentOld := "agent-old"
@@ -271,7 +253,7 @@ func TestTaskMergeRuntime(t *testing.T) {
 			Value: &mesos1Older,
 		},
 	}
-	tt := initializeTask(nil, jobID, instID, runtime)
+	tt := initializeTask(suite.taskStore, suite.jobID, suite.instanceID, runtime)
 
 	curTime := time.Now().UTC().Format(time.RFC3339Nano)
 
@@ -303,112 +285,88 @@ func TestTaskMergeRuntime(t *testing.T) {
 	}
 
 	mergedRuntime := tt.mergeRuntime(newRuntime)
-	validateMergeRuntime(t, newRuntime, mergedRuntime, uint64(3))
+	validateMergeRuntime(suite, newRuntime, mergedRuntime, uint64(3))
 }
 
 // TestTaskUpdateRuntime tests updating the task runtime without any DB errors
-func TestTaskUpdateRuntime(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	taskstore := storemocks.NewMockTaskStore(ctrl)
-	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
-	instID := uint32(1)
+func (suite *TaskTestSuite) TestTaskUpdateRuntime() {
 	runtime := initializeTaskRuntime(pbtask.TaskState_LAUNCHED, 2)
 	runtime.GoalState = pbtask.TaskState_SUCCEEDED
-	tt := initializeTask(taskstore, jobID, instID, runtime)
+	tt := initializeTask(suite.taskStore, suite.jobID, suite.instanceID, runtime)
 
 	newRuntime := &pbtask.RuntimeInfo{
 		State: pbtask.TaskState_RUNNING,
 	}
 
-	taskstore.EXPECT().
-		UpdateTaskRuntime(gomock.Any(), jobID, instID, gomock.Any()).
+	suite.taskStore.EXPECT().
+		UpdateTaskRuntime(gomock.Any(), suite.jobID, suite.instanceID, gomock.Any()).
 		Do(func(ctx context.Context, jobID *peloton.JobID, instanceID uint32, runtime *pbtask.RuntimeInfo) {
-			assert.Equal(t, runtime.GetState(), pbtask.TaskState_RUNNING)
-			assert.Equal(t, runtime.Revision.Version, uint64(3))
-			assert.Equal(t, runtime.GetGoalState(), pbtask.TaskState_SUCCEEDED)
+			suite.Equal(runtime.GetState(), pbtask.TaskState_RUNNING)
+			suite.Equal(runtime.Revision.Version, uint64(3))
+			suite.Equal(runtime.GetGoalState(), pbtask.TaskState_SUCCEEDED)
 		}).
 		Return(nil)
 
 	err := tt.UpdateRuntime(context.Background(), newRuntime, UpdateCacheAndDB)
-	assert.Nil(t, err)
+	suite.Nil(err)
 }
 
 // TestTaskUpdateRuntimeWithNoRuntimeInCache tests updating task runtime when
 // the runtime in cache is nil
-func TestTaskUpdateRuntimeWithNoRuntimeInCache(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	taskstore := storemocks.NewMockTaskStore(ctrl)
-	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
-	instID := uint32(1)
+func (suite *TaskTestSuite) TestTaskUpdateRuntimeWithNoRuntimeInCache() {
 	runtime := initializeTaskRuntime(pbtask.TaskState_LAUNCHED, 2)
 	runtime.GoalState = pbtask.TaskState_SUCCEEDED
-	tt := initializeTask(taskstore, jobID, instID, nil)
+	tt := initializeTask(suite.taskStore, suite.jobID, suite.instanceID, nil)
 
 	newRuntime := &pbtask.RuntimeInfo{
 		State: pbtask.TaskState_RUNNING,
 	}
 
-	taskstore.EXPECT().
-		GetTaskRuntime(gomock.Any(), jobID, instID).Return(runtime, nil)
+	suite.taskStore.EXPECT().
+		GetTaskRuntime(gomock.Any(), suite.jobID, suite.instanceID).Return(runtime, nil)
 
-	taskstore.EXPECT().
-		UpdateTaskRuntime(gomock.Any(), jobID, instID, gomock.Any()).
+	suite.taskStore.EXPECT().
+		UpdateTaskRuntime(gomock.Any(), suite.jobID, suite.instanceID, gomock.Any()).
 		Do(func(ctx context.Context, jobID *peloton.JobID, instanceID uint32, runtime *pbtask.RuntimeInfo) {
-			assert.Equal(t, runtime.GetState(), pbtask.TaskState_RUNNING)
-			assert.Equal(t, runtime.Revision.Version, uint64(3))
-			assert.Equal(t, runtime.GetGoalState(), pbtask.TaskState_SUCCEEDED)
+			suite.Equal(runtime.GetState(), pbtask.TaskState_RUNNING)
+			suite.Equal(runtime.Revision.Version, uint64(3))
+			suite.Equal(runtime.GetGoalState(), pbtask.TaskState_SUCCEEDED)
 		}).
 		Return(nil)
 
 	err := tt.UpdateRuntime(context.Background(), newRuntime, UpdateCacheAndDB)
-	assert.Nil(t, err)
+	suite.Nil(err)
 }
 
 // TestTaskUpdateRuntimeDBError tests updating the task runtime with DB errors
-func TestTaskUpdateRuntimeDBError(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	taskstore := storemocks.NewMockTaskStore(ctrl)
-	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
-	instID := uint32(1)
+func (suite *TaskTestSuite) TestTaskUpdateRuntimeDBError() {
 	runtime := initializeTaskRuntime(pbtask.TaskState_LAUNCHED, 2)
 	runtime.GoalState = pbtask.TaskState_SUCCEEDED
-	tt := initializeTask(taskstore, jobID, instID, runtime)
+	tt := initializeTask(suite.taskStore, suite.jobID, suite.instanceID, runtime)
 
 	newRuntime := &pbtask.RuntimeInfo{
 		State: pbtask.TaskState_RUNNING,
 	}
 
-	taskstore.EXPECT().
-		UpdateTaskRuntime(gomock.Any(), jobID, instID, gomock.Any()).
+	suite.taskStore.EXPECT().
+		UpdateTaskRuntime(gomock.Any(), suite.jobID, suite.instanceID, gomock.Any()).
 		Return(fmt.Errorf("fake db error"))
 
 	err := tt.UpdateRuntime(context.Background(), newRuntime, UpdateCacheAndDB)
-	assert.NotNil(t, err)
+	suite.NotNil(err)
 }
 
 // TestTaskUpdateRuntimeInCacheOnly tests updating runtime in the cache only
-func TestTaskUpdateRuntimeInCacheOnly(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	taskstore := storemocks.NewMockTaskStore(ctrl)
-	jobID := &peloton.JobID{Value: uuid.NewRandom().String()}
-	instID := uint32(1)
+func (suite *TaskTestSuite) TestTaskUpdateRuntimeInCacheOnly() {
 	runtime := initializeTaskRuntime(pbtask.TaskState_LAUNCHED, 2)
 	runtime.GoalState = pbtask.TaskState_SUCCEEDED
-	tt := initializeTask(taskstore, jobID, instID, runtime)
+	tt := initializeTask(suite.taskStore, suite.jobID, suite.instanceID, runtime)
 
 	newRuntime := &pbtask.RuntimeInfo{
 		State: pbtask.TaskState_RUNNING,
 	}
 
 	err := tt.UpdateRuntime(context.Background(), newRuntime, UpdateCacheOnly)
-	assert.Nil(t, err)
-	assert.Equal(t, tt.runtime.GetState(), pbtask.TaskState_RUNNING)
+	suite.Nil(err)
+	suite.Equal(tt.runtime.GetState(), pbtask.TaskState_RUNNING)
 }
