d6975fa4ceafe5eb784a19e8b3077f61011d0e98
Sachin Sharma <sachins@uber.com>
2018-07-31 15:21:17 -0700

    Increase code coverage for hostmgr/queue
    Summary: T1831817 - This diff increases code coverage for hostmgr/queue from 85% to 100%
    
    Reviewers: #peloton, varung
    
    Reviewed By: #peloton, varung
    
    Subscribers: avyas, varung, jenkins
    
    Differential Revision: https://code.uberinternal.com/D1945505

--- hostmgr/queue/maintenance_test.go
+++ hostmgr/queue/maintenance_test.go
@@ -1,21 +1,27 @@
 package queue
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
+	"code.uber.internal/infra/peloton/common/queue/mocks"
+
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/suite"
 )
 
 type MaintenanceQueueTestSuite struct {
 	suite.Suite
-	maintenanceQueue MaintenanceQueue
-	maxWaitTime      time.Duration
+	mockCtrl      *gomock.Controller
+	maxWaitTime   time.Duration
+	testHostnames []string
 }
 
 func (suite *MaintenanceQueueTestSuite) SetupSuite() {
-	suite.maintenanceQueue = NewMaintenanceQueue()
+	suite.mockCtrl = gomock.NewController(suite.T())
 	suite.maxWaitTime = 1
+	suite.testHostnames = []string{"testHost1", "testHost2"}
 }
 
 func TestMaintenanceQueue(t *testing.T) {
@@ -23,18 +29,43 @@ func TestMaintenanceQueue(t *testing.T) {
 }
 
 func (suite *MaintenanceQueueTestSuite) TestMaintenanceQueue_EnqueueDequeue() {
-	testHostnames := []string{"testHost1", "testHost2"}
-	err := suite.maintenanceQueue.Enqueue(testHostnames)
+	maintenanceQueue := NewMaintenanceQueue()
+	err := maintenanceQueue.Enqueue(suite.testHostnames)
 	suite.NoError(err)
 
-	for i := range testHostnames {
-		h, err := suite.maintenanceQueue.Dequeue(suite.maxWaitTime * time.Second)
-		suite.NoError(err)
-		suite.Equal(testHostnames[i], h)
+	// Check length
+	suite.Equal(len(suite.testHostnames), maintenanceQueue.Length())
+
+	hostnameMap := make(map[string]bool)
+	for _, hostname := range suite.testHostnames {
+		hostnameMap[hostname] = true
 	}
+	for range suite.testHostnames {
+		h, err := maintenanceQueue.Dequeue(suite.maxWaitTime * time.Second)
+		suite.NoError(err)
+		suite.True(hostnameMap[h])
+		delete(hostnameMap, h)
+	}
+	suite.Equal(0, len(hostnameMap))
+}
+
+func (suite *MaintenanceQueueTestSuite) TestMaintenanceQueue_QueueErrors() {
+	queue := mocks.NewMockQueue(suite.mockCtrl)
+	maintenanceQueue := &maintenanceQueue{
+		queue: queue,
+	}
+	// Test Enqueue error
+	queue.EXPECT().Enqueue(gomock.Any()).
+		Return(fmt.Errorf("fake enqueue error")).
+		Times(len(suite.testHostnames))
+	err := maintenanceQueue.Enqueue(suite.testHostnames)
+	suite.Error(err)
 
 	// Test Dequeue error
-	hostname, err := suite.maintenanceQueue.Dequeue(suite.maxWaitTime * time.Second)
+	queue.EXPECT().Dequeue(gomock.Any()).
+		Return(nil, fmt.Errorf("fake dequeue error"))
+	hostname, err := maintenanceQueue.
+		Dequeue(suite.maxWaitTime * time.Second)
 	suite.Error(err)
 	suite.Equal("", hostname)
 }
