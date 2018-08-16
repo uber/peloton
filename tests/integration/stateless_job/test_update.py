25158e1d7c3738bc3a21562774f221b5772ae349
Zhixin Wen <zhixin@uber.com>
2018-08-15 17:56:59 -0700

    Fix update integration test
    Summary:
    update integration tests should run by default.
    TaskInfos returned by tasksvc.List does not include
    task config, so removing checking on task config.
    
    Reviewers: mu, apoorvaj, #peloton
    
    Reviewed By: apoorvaj, #peloton
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1989349

--- tests/integration/stateless_job/test_update.py
+++ tests/integration/stateless_job/test_update.py
@@ -2,7 +2,7 @@ import pytest
 
 from tests.integration.update import Update
 
-pytestmark = [pytest.mark.stateless, pytest.mark.update]
+pytestmark = [pytest.mark.default, pytest.mark.stateless, pytest.mark.update]
 
 UPDATE_STATELESS_JOB_FILE = "test_update_stateless_job.yaml"
 UPDATE_STATELESS_JOB_ADD_INSTANCES_FILE = \
@@ -20,7 +20,7 @@ def test__create_update(stateless_job):
     update.create()
     update.wait_for_state(goal_state='SUCCEEDED')
     new_task_infos = stateless_job.list_tasks().value
-    assert_task_update_change_applied(old_task_infos, new_task_infos)
+    assert_task_mesos_id_changed(old_task_infos, new_task_infos)
 
 
 def test__create_update_add_instances(stateless_job):
@@ -47,7 +47,7 @@ def test__create_update_update_and_add_instances(stateless_job):
     new_task_infos = stateless_job.list_tasks().value
     assert len(old_task_infos) == 3
     assert len(new_task_infos) == 5
-    assert_task_update_change_applied(old_task_infos, new_task_infos)
+    assert_task_mesos_id_changed(old_task_infos, new_task_infos)
 
 
 def test__create_update_with_batch_size(stateless_job):
@@ -60,7 +60,7 @@ def test__create_update_with_batch_size(stateless_job):
     update.create()
     update.wait_for_state(goal_state='SUCCEEDED')
     new_task_infos = stateless_job.list_tasks().value
-    assert_task_update_change_applied(old_task_infos, new_task_infos)
+    assert_task_mesos_id_changed(old_task_infos, new_task_infos)
 
 
 def test__create_update_add_instances_with_batch_size(stateless_job):
@@ -89,7 +89,7 @@ def test__create_update_update_and_add_instances_with_batch(stateless_job):
     new_task_infos = stateless_job.list_tasks().value
     assert len(old_task_infos) == 3
     assert len(new_task_infos) == 5
-    assert_task_update_change_applied(old_task_infos, new_task_infos)
+    assert_task_mesos_id_changed(old_task_infos, new_task_infos)
 
 
 def assert_task_mesos_id_changed(old_task_infos, new_task_infos):
@@ -101,21 +101,3 @@ def assert_task_mesos_id_changed(old_task_infos, new_task_infos):
         old_mesos_id = old_task_info.runtime.mesosTaskId.value
         new_mesos_id = new_task_infos[instance_id].runtime.mesosTaskId.value
         assert old_mesos_id != new_mesos_id
-
-
-def assert_task_cmd_changed(old_task_infos, new_task_infos):
-    """""
-    assert if the cmd in old_task_infos changes in new_task_infos
-    """
-    for instance_id, old_task_info in old_task_infos.items():
-        old_config = old_task_info.config
-        new_config = new_task_infos[instance_id].config
-        assert old_config.command.value != new_config.command.value
-
-
-def assert_task_update_change_applied(old_task_infos, new_task_infos):
-    """""
-    assert if the update makes all of the changes expected
-    """
-    assert_task_mesos_id_changed(old_task_infos, new_task_infos)
-    assert_task_cmd_changed(old_task_infos, new_task_infos)
