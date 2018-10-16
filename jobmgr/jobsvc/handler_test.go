a80551574793720af8ff269089c069434cd17ff2
Varun Gupta <varung@uber.com>
2018-10-16 16:29:37 -0700

    Converge recent job config version for all batch tasks
    Summary: For batch jobs, converge most recent job config version (increment on job update) to all tasks such that previous job configs can be deleted to prevent Cassandra large partition for job_config table.
    
    Test Plan:
    unit test
    pcluster
    preprod
    
    Reviewers: apoorvaj, mabansal, zhixin, O521 Project Peloton: Add reviewers
    
    Reviewed By: apoorvaj, zhixin, O521 Project Peloton: Add reviewers
    
    Subscribers: jenkins
    
    Maniphest Tasks: T2030081
    
    Differential Revision: https://code.uberinternal.com/D2168347

--- jobmgr/jobsvc/handler_test.go
+++ jobmgr/jobsvc/handler_test.go
@@ -23,6 +23,7 @@ import (
 	"code.uber.internal/infra/peloton/jobmgr/cached"
 	cachedmocks "code.uber.internal/infra/peloton/jobmgr/cached/mocks"
 	cachedtest "code.uber.internal/infra/peloton/jobmgr/cached/test"
+	jobmgrcommon "code.uber.internal/infra/peloton/jobmgr/common"
 	goalstatemocks "code.uber.internal/infra/peloton/jobmgr/goalstate/mocks"
 	jobmgrtask "code.uber.internal/infra/peloton/jobmgr/task"
 	leadermocks "code.uber.internal/infra/peloton/leader/mocks"
@@ -935,6 +936,16 @@ func (suite *JobHandlerTestSuite) TestJobScaleUp() {
 				Version: 2,
 			},
 		}, nil)
+	suite.mockedCachedJob.EXPECT().
+		PatchTasks(gomock.Any(), gomock.Any()).
+		Do(func(ctx context.Context, runtimeDiffs map[uint32]jobmgrcommon.RuntimeDiff) {
+			suite.Equal(uint32(len(runtimeDiffs)), oldJobConfig.GetInstanceCount())
+			for _, runtimeDiff := range runtimeDiffs {
+				suite.Equal(runtimeDiff[jobmgrcommon.ConfigVersionField].(uint64),
+					uint64(2))
+			}
+		}).
+		Return(nil)
 	req := &job.UpdateRequest{
 		Id:     jobID,
 		Config: newJobConfig,
@@ -993,6 +1004,16 @@ func (suite *JobHandlerTestSuite) TestJobUpdateInstanceConfig() {
 		}, nil)
 	suite.mockedCachedJob.EXPECT().
 		Update(gomock.Any(), gomock.Any(), gomock.Any(), cached.UpdateCacheAndDB).
+		Return(nil)
+	suite.mockedCachedJob.EXPECT().
+		PatchTasks(gomock.Any(), gomock.Any()).
+		Do(func(ctx context.Context, runtimeDiffs map[uint32]jobmgrcommon.RuntimeDiff) {
+			suite.Equal(uint32(len(runtimeDiffs)), oldJobConfig.GetInstanceCount())
+			for _, runtimeDiff := range runtimeDiffs {
+				suite.Equal(runtimeDiff[jobmgrcommon.ConfigVersionField].(uint64),
+					uint64(2))
+			}
+		}).
 		Return(nil)
 	suite.mockedGoalStateDriver.EXPECT().
 		EnqueueJob(gomock.Any(), gomock.Any()).
