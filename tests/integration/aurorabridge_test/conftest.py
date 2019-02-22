c67da88b8d2006d11e76f24cd189285516a46176
Kevin Xu <kevinxu@uber.com>
2019-02-22 14:04:38 -0800

    Wait for deletion to finish for each aurora bridge integration test
    Summary: Wait for deletion to finish for each aurora bridge integration test
    
    Reviewers: codyg, O521 Project Peloton: Add reviewers
    
    Reviewed By: codyg
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D2538917

--- tests/integration/aurorabridge_test/conftest.py
+++ tests/integration/aurorabridge_test/conftest.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 from tests.integration.aurorabridge_test.client import Client
@@ -11,12 +13,23 @@ def client():
     yield client
 
     # Delete all jobs
-    delete_jobs()
+    _delete_jobs()
 
 
-def delete_jobs(respool_path='/AuroraBridge'):
+def _delete_jobs(respool_path='/AuroraBridge',
+                 timeout_secs=240):
     resp = query_jobs(respool_path)
 
     for j in resp.records:
         job = StatelessJob(job_id=j.job_id.value)
         job.delete(force_delete=True)
+
+    # wait for job deletion to complete
+    deadline = time.time() + timeout_secs
+    while time.time() < deadline:
+        resp = query_jobs(respool_path)
+        if len(resp.records) == 0:
+            return
+        time.sleep(2)
+
+    assert False, 'timed out waiting for jobs to be deleted'
