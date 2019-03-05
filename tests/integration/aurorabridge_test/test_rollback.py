75de451d29339a488f4ca114044326556eb72442
Varun Gupta <varung@uber.com>
2019-03-05 11:45:36 -0800

    Integ test auto rollback aurora bridge
    Reviewers: codyg, kevinxu, O521 Project Peloton: Add reviewers
    
    Reviewed By: kevinxu, O521 Project Peloton: Add reviewers
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D2570173

---
+++ tests/integration/aurorabridge_test/test_rollback.py
@@ -0,0 +1,48 @@
+import pytest
+
+from tests.integration.aurorabridge_test.client import api
+from tests.integration.aurorabridge_test.util import (
+    get_job_update_request,
+    start_job_update,
+    wait_for_rolled_back,
+)
+
+pytestmark = [pytest.mark.default,
+              pytest.mark.aurorabridge,
+              pytest.mark.random_order(disabled=True)]
+
+
+def test__simple_auto_rolled_back(client):
+    """
+    Create a job, then issue a bad config update and validate
+    job is rolled back to previous version
+    """
+    start_job_update(
+        client,
+        'test_dc_labrat.yaml',
+        'start job update test/dc/labrat')
+
+    res = client.start_job_update(
+        get_job_update_request('test_dc_labrat_bad_config.yaml'),
+        'rollout bad config')
+    wait_for_rolled_back(client, res.key)
+
+    # validate job is rolled back to previous config
+    res = client.get_tasks_without_configs(api.TaskQuery(
+        jobKeys={res.key.job},
+        statuses={api.ScheduleStatus.RUNNING}
+    ))
+
+    tasks = res.tasks
+    assert len(tasks) == 3
+
+    for t in tasks:
+        for r in t.assignedTask.task.resources:
+            if r.numCpus > 0:
+                assert r.numCpus == 0.25
+            elif r.ramMb > 0:
+                assert r.ramMb == 32
+            elif r.diskMb > 0:
+                assert r.diskMb == 128
+            else:
+                assert False, 'unexpected resource {}'.format(r)
