1141ac58d5c9cf703517d7d20dc2703ecf1b8bfe
Varun Gupta <varung@uber.com>
2019-03-05 11:48:22 -0800

    Integ test revocable job aurora bridge
    Reviewers: codyg, kevinxu, O521 Project Peloton: Add reviewers
    
    Reviewed By: kevinxu, O521 Project Peloton: Add reviewers
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D2570205

--- tests/integration/aurorabridge_test/test_update.py
+++ tests/integration/aurorabridge_test/test_update.py
@@ -35,3 +35,34 @@ def test__start_job_update_with_pulse(client):
             api.JobUpdateStatus.ROLLING_FORWARD,
         },
         api.JobUpdateStatus.ROLLED_FORWARD)
+
+
+def test__start_job_update_revocable_job(client):
+    """
+    Given 12 non-revocable cpus, and 12 revocable cpus
+    Create a non-revocable of 10 instance, with 1 CPU per instance
+    Create a revocable job of 3 instance, with 1 CPU per instance
+    """
+    non_revocable_job = start_job_update(
+        client,
+        'test_dc_labrat_large.yaml',
+        'start job update test/dc/labrat_large')
+
+    # validate 10 non-revocable tasks are running
+    res = client.get_tasks_without_configs(api.TaskQuery(
+        jobKeys={non_revocable_job},
+        statuses={api.ScheduleStatus.RUNNING}
+    ))
+    assert len(res.tasks) == 10
+
+    revocable_job = start_job_update(
+        client,
+        'test_dc_labrat_revocable.yaml',
+        'start job update test/dc/labrat_revocable')
+
+    # validate 3 revocable tasks are running
+    res = client.get_tasks_without_configs(api.TaskQuery(
+        jobKeys={revocable_job},
+        statuses={api.ScheduleStatus.RUNNING}
+    ))
+    assert len(res.tasks) == 3
