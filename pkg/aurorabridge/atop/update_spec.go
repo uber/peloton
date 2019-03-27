76371b27f90a9600af2c403608bb4befe938a566
Kevin Xu <kevinxu@uber.com>
2019-03-26 19:16:21 -0700

    Fix max_tolerable_instance_failures and max_instances_retries for aurora bridge
    Summary:
    In Peloton max_tolerable_instance_failures means number of instances to
    fail to trigger an update failure, it's slightly different than Aurora's
    maxFailedInstances - meaning number of instances allowed to fail before
    triggering an update failure. Increment maxFailedInstances by one to
    correctly set max_tolerable_instance_failures.
    
    In Peloton max_instances_retries means number of times
    an instance will be retried before failing the instance,
    however in Aurora maxPerInstanceFailures means maximum
    instance failures will be tolerated, so we need to increment
    by 1.
    
    Reviewers: varung, codyg, apoorvaj, zhixin
    
    Reviewed By: varung
    
    Subscribers: jenkins
    
    Maniphest Tasks: T2649455
    
    Differential Revision: https://code.uberinternal.com/D2617123

--- pkg/aurorabridge/atop/update_spec.go
+++ pkg/aurorabridge/atop/update_spec.go
@@ -22,11 +22,21 @@ import (
 // NewUpdateSpec creates a new UpdateSpec.
 func NewUpdateSpec(s *api.JobUpdateSettings) *stateless.UpdateSpec {
 	return &stateless.UpdateSpec{
-		BatchSize:                    uint32(s.GetUpdateGroupSize()),
-		RollbackOnFailure:            s.GetRollbackOnFailure(),
-		MaxInstanceRetries:           uint32(s.GetMaxPerInstanceFailures()),
-		MaxTolerableInstanceFailures: uint32(s.GetMaxFailedInstances()),
-		StartPods:                    true,
+		BatchSize:         uint32(s.GetUpdateGroupSize()),
+		RollbackOnFailure: s.GetRollbackOnFailure(),
+		StartPods:         true,
+
+		// In Peloton max_instances_retries means number of times
+		// an instance will be retried before failing the instance,
+		// however in Aurora maxPerInstanceFailures means maximum
+		// instance failures will be tolerated, so we need to increment
+		// by 1.
+		MaxInstanceRetries: uint32(s.GetMaxPerInstanceFailures() + 1),
+
+		// In Peloton max_tolerable_instance_failures means number of
+		// instances to fail to trigger an update failure, so we need to
+		// increment maxFailedInstances from Aurora by 1
+		MaxTolerableInstanceFailures: uint32(s.GetMaxFailedInstances() + 1),
 
 		// Peloton does not support pulsed updates, so if block if no pulse is
 		// set, then we start the update in a paused state such that it must
--- pkg/aurorabridge/ptoa/job_update_settings.go
+++ pkg/aurorabridge/ptoa/job_update_settings.go
@@ -24,10 +24,21 @@ import (
 // NewJobUpdateSettings converts UpdateSpec to aurora JobUpdateSettings.
 func NewJobUpdateSettings(u *stateless.UpdateSpec) *api.JobUpdateSettings {
 	return &api.JobUpdateSettings{
-		UpdateGroupSize:        ptr.Int32(int32(u.GetBatchSize())),
-		RollbackOnFailure:      ptr.Bool(u.GetRollbackOnFailure()),
-		MaxPerInstanceFailures: ptr.Int32(int32(u.GetMaxInstanceRetries())),
-		MaxFailedInstances:     ptr.Int32(int32(u.GetMaxTolerableInstanceFailures())),
+		UpdateGroupSize:   ptr.Int32(int32(u.GetBatchSize())),
+		RollbackOnFailure: ptr.Bool(u.GetRollbackOnFailure()),
+
+		// In Peloton max_instances_retries means number of times
+		// an instance will be retried before failing the instance,
+		// however in Aurora maxPerInstanceFailures means maximum
+		// instance failures will be tolerated, so we need to decrement
+		// by 1.
+		MaxPerInstanceFailures: ptr.Int32(int32(u.GetMaxInstanceRetries()) - 1),
+
+		// In Peloton max_tolerable_instance_failures means number of
+		// instances to fail to trigger an update failure, so we need to
+		// decrement it by 1 when converting to maxFailedInstances from
+		// Aurora
+		MaxFailedInstances: ptr.Int32(int32(u.GetMaxTolerableInstanceFailures()) - 1),
 
 		// Skip mapping to BlockIfNoPulsesAfterMs since we don't have
 		// enough information in UpdateSpec
