e2f024306b72e2dad4ecb79222c910ab39d7467e
Kevin Xu <kevinxu@uber.com>
2019-05-29 17:38:47 -0700

    Add failure test for getJobUpdateDetails
    Summary: Add failure test for getJobUpdateDetails while jobmgr is restarting
    
    Reviewers: varung, O521 Project Peloton: Add reviewers
    
    Reviewed By: varung, O521 Project Peloton: Add reviewers
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D2888895

--- tests/integration/aurorabridge_test/test_failure.py
+++ tests/integration/aurorabridge_test/test_failure.py
@@ -1,15 +1,20 @@
+import logging
 import pytest
-import time
 import random
+import time
 
 from tests.integration.aurorabridge_test.client import api
 from tests.integration.aurorabridge_test.util import (
     get_job_update_request,
     wait_for_rolled_forward,
+    wait_for_running,
+    wait_for_update_status,
 )
 
 pytestmark = [pytest.mark.default,
               pytest.mark.aurorabridge]
+
+log = logging.getLogger(__name__)
 
 
 def test__simple_update_with_restart_component(
@@ -130,3 +135,78 @@ def test__simple_update_tasks_reconcile(
         statuses={api.ScheduleStatus.RUNNING}
     ))
     assert len(res.tasks) == 10
+
+
+def test__get_job_update_details__restart_jobmgr(client,
+                                                 jobmgr,
+                                                 resmgr,
+                                                 hostmgr,
+                                                 mesos_master):
+    """
+    Start an update, call getJobUpdateDetails, restart jobmgr:
+    1. Before recovery finishes, expect error
+    2. After recovery finishes, expect getJobUpdateDetails to include the
+       correct job
+    """
+    # start job update paused
+    req = get_job_update_request('test_dc_labrat_large_job.yaml')
+    req.settings.updateGroupSize = 10
+    req.settings.blockIfNoPulsesAfterMs = 604800000
+    res = client.start_job_update(req,
+                                  'start job update test/dc/labrat_large_job')
+
+    job_update_key = res.key
+    job_key = job_update_key.job
+    wait_for_update_status(
+        client,
+        job_update_key,
+        {api.JobUpdateStatus.ROLLING_FORWARD},
+        api.JobUpdateStatus.ROLL_FORWARD_AWAITING_PULSE,
+        120,
+    )
+
+    def check():
+        res = client.get_job_update_details(None,
+                                            api.JobUpdateQuery(jobKey=job_key))
+        assert len(res.detailsList) == 1
+        assert res.detailsList[0].update.summary.key.job == job_key
+
+    def wait():
+        start = time.time()
+        while time.time() - start < 120:
+            try:
+                check()
+                break
+            except Exception as e:
+                log.info('getJobUpdateDetails failed: %s, retrying...', e)
+                time.sleep(0.5)
+        else:
+            assert False, \
+                'Timed out waiting for getJobUpdateDetails endpoint to recover'
+
+    # verify getJobUpdateDetailsResult
+    check()
+
+    # restart jobmgr
+    jobmgr.restart()
+    wait()
+
+    # wait additional time before proceeding, so that jobmgr has leader elected
+    log.info('Wating 5 seconds before proceeding')
+    time.sleep(5)
+
+    # resume update
+    client.pulse_job_update(job_update_key)
+    wait_for_rolled_forward(client, job_update_key)
+    wait_for_running(client, job_key)
+
+    # verify getJobUpdateDetailsResult
+    check()
+
+    # restart jobmgr
+    jobmgr.restart()
+    wait()
+
+    # wait additional time before exiting, so that jobmgr has leader elected
+    log.info('Wating 5 seconds before exiting')
+    time.sleep(5)
