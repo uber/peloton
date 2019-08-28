uber/peloton

commit 2be1989008ecfae17d0061c549e2b58f2bad472d

Refactor use of UpdateRecord in GetUpdatesForJob in store.go
Summary:
GetUpdatesForJob returns a list of update_ids for a provided job. It deserialized the return result set in UpdateRecord struct which has 20 fields. STAPI layer uses reflection on fields for UpdateRecord which is unnecessary since we only three fields are returned from Updates View. Adding a new UpdateViewRecord which has three fields as Updates MV.

CPU Profiling, reflects 21% time is spent in FillObject for Updates of which most of the time is in reflection calls on UpdateRecord.
{F151015519}

Reviewers: apoorvaj, zhixin, sachins, O521 Project Peloton: Add reviewers

Reviewed By: sachins, O521 Project Peloton: Add reviewers

Subscribers: jenkins

Differential Revision: https://code.uberinternal.com/D3203283

pkg/storage/cassandra/models.go
@@ -143,6 +143,13 @@ type FrameworkInfoRecord struct {
 
 // Resource pool (to be added)
 
+// UpdateViewRecord tracks the job update info from materialized view
+type UpdateViewRecord struct {
+	UpdateID     querybuilder.UUID `cql:"update_id"`
+	JobID        querybuilder.UUID `cql:"job_id"`
+	CreationTime time.Time         `cql:"creation_time"`
+}
+
 // UpdateRecord tracks the job update info
 type UpdateRecord struct {
 	UpdateID             querybuilder.UUID `cql:"update_id"`

pkg/storage/cassandra/store.go
@@ -2784,7 +2784,7 @@ func (s *Store) GetUpdatesForJob(
 	var updateList []*SortUpdateInfoTS
 
 	queryBuilder := s.DataStore.NewQuery()
-	stmt := queryBuilder.Select("*").From(updatesByJobView).
+	stmt := queryBuilder.Select("update_id").From(updatesByJobView).
 		Where(qb.Eq{"job_id": jobID})
 	allResults, err := s.executeRead(ctx, stmt)
 	if err != nil {
@@ -2796,7 +2796,7 @@ func (s *Store) GetUpdatesForJob(
 	}
 
 	for _, value := range allResults {
-		var record UpdateRecord
+		var record UpdateViewRecord
 		err := FillObject(value, &record, reflect.TypeOf(record))
 		if err != nil {
 			log.WithError(err).

commit 06b15ede2aa0b2df6033957a4f7e8eab87fbb1f9

Fix GetUpdatesForJob in store.go
Summary:
This diff does the following
1. Reverts commit cbcd027fb3f4eec9e6989bdb07a41471b493f3be.
2. Fix issue introduced by commit 2be1989008ecfae17d0061c549e2b58f2bad472d. `UpdateViewRecord` has 3 fields - updateID, jobID and creationTime - but we were querying DB for only updateID which caused the other 2 fields to be nil.

Test Plan: Tested the fix on test cluster

Reviewers: kevinxu, O521 Project Peloton: Add reviewers

Reviewed By: kevinxu, O521 Project Peloton: Add reviewers

Subscribers: jenkins

Tags: #peloton

Differential Revision: https://code.uberinternal.com/D3236723

pkg/storage/cassandra/models.go
@@ -143,6 +143,13 @@ type FrameworkInfoRecord struct {
 
 // Resource pool (to be added)
 
+// UpdateViewRecord tracks the job update info from materialized view
+type UpdateViewRecord struct {
+	UpdateID     querybuilder.UUID `cql:"update_id"`
+	JobID        querybuilder.UUID `cql:"job_id"`
+	CreationTime time.Time         `cql:"creation_time"`
+}
+
 // UpdateRecord tracks the job update info
 type UpdateRecord struct {
 	UpdateID             querybuilder.UUID `cql:"update_id"`

pkg/storage/cassandra/store.go
@@ -28,7 +28,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/uber/peloton/.gen/mesos/v1"
+	mesos_v1 "github.com/uber/peloton/.gen/mesos/v1"
 	"github.com/uber/peloton/.gen/peloton/api/v0/job"
 	"github.com/uber/peloton/.gen/peloton/api/v0/peloton"
 	"github.com/uber/peloton/.gen/peloton/api/v0/query"
@@ -2777,7 +2777,8 @@ func (s *Store) GetUpdatesForJob(
 	var updateList []*SortUpdateInfoTS
 
 	queryBuilder := s.DataStore.NewQuery()
-	stmt := queryBuilder.Select("*").From(updatesByJobView).
+	stmt := queryBuilder.Select("update_id", "job_id", "creation_time").
+		From(updatesByJobView).
 		Where(qb.Eq{"job_id": jobID})
 	allResults, err := s.executeRead(ctx, stmt)
 	if err != nil {
@@ -2789,7 +2790,7 @@ func (s *Store) GetUpdatesForJob(
 	}
 
 	for _, value := range allResults {
-		var record UpdateRecord
+		var record UpdateViewRecord
 		err := FillObject(value, &record, reflect.TypeOf(record))
 		if err != nil {
 			log.WithError(err).